cbf1c9257bf304eb39bfa72f0a0483c593ed1b15
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-09 16:09:38 -0700

    Program that prints with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print string using printf()
+ *
+ * description: using main function, this programme prints exactly
+ * "with proper grammar, but the outcome is a piece of art,"
+ *
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
