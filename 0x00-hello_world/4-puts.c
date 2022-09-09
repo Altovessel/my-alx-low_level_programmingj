1a2cf9a310d21cfbb8be83d6c55f171520738295
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-09 15:12:57 -0700

    A program that prints out string with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - print the string in the puts funtion
+ * main - print the string using the puts function
  *
- * Description: using main function
- * this program prints out "Programming is like building a multilingual puzzle
- * return: 0
+ * description: using the main function, this program prints out exactly
+ * "Programming is like building a multilingual puzzle"
+ *
+ * return: 0(always success)
  */
 int main(void)
 {
