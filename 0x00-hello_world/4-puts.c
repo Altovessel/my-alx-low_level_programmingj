8c76634bb23c6727aa62f2a92d62430afac14923
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-09 17:13:46 -0700

    Program that prints with the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - print the string using the puts function
  *
- * description: using the main function, this program prints out exactly
+ * Description: using the main function, this program prints out exactly
  * "Programming is like building a multilingual puzzle"
  *
- * return: 0(always success)
+ * Return: 0(always success)
  */
-int main(void)
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
