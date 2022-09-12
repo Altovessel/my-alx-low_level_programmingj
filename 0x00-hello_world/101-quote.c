e93ce33ca12962c7dc3a6554ff309243fc055718
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-11 19:02:23 -0700

    Program tat prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the main function, this program prints "and that piece
+ * of art is useful" - Dora Korpar, 2015-10-19 to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	/*Declarations*/
+	char full_name = ' - Dora Korpar';
+	int date = "%F";
+	long char My_var = 'and that piece of art is useful\"';
+
+	/*Statements*/
+	putchar(str(My_var, full_name, date));
+	stderr
+
+	Return(1);
+}
