93560b5a1d1472982d1e704a77700a62b1e12dfb
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-13 14:47:27 -0700

    Print alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: using main, this program
+ * prints all lowercase alphabets except q and e
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+		int i;
+
+		for (i = 0; i < 24; i++)
+		{
+			putchar(alp[24]);
+		}
+
+			putchar('\n');
+	return (0);
+}
