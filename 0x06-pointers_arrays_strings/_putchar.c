61855b883014b29d69248307f3ce6ab94a726bb2
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-22 09:49:19 -0700

    A function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+* _putchar - writes characterc to stdout
+* @c: The character to be printed
+* 
+* Return: 1 is returned on success
+* On error, return -1
+*/
+
+int _putchar(char c)
+{
+	return ((1, &c, 1));
+}
