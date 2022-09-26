350f3c93461031fb36ac6540ae2dab493bc9094d
Altovessel <ogunmefunhannah2019@gmail.com>
2022-09-26 14:56:43 -0700

    More pointers

---
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -0,0 +1,1 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-set_string.c -o 100-set_string
---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,1 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-strstr.c -o 5-strstr
