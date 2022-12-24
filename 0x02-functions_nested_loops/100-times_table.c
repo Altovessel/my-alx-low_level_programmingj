f820d701f01dda84f668c1bc33c0f7e7b5e0aa78
Altovessel <ogunmefunhannah2019@gmail.com>
2022-12-24 01:40:53 -0800

    New commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,44 +3,33 @@
 /**
  * print_times_table - Prints the n times table
  *
- * @n: number times table (0 < n <= 15)
+ * @n: number of times table (0 < n <= 15)
  *
  * Return: no return
  */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int n, p, mul;
 
-	if (n >= 0 && n <= 15)
+	for (n = 0; n <= 15; n++)
 	{
-		for (a = 0; a <= n; a++)
+		_putchar(48);
+		for (p = 1; p <= n; p++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+			mul = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (mul <= 15)
 			{
-				op = a * b;
-				_putchar(44);
 				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
+				_putchar(mul + 48);
 			}
-			_putchar('\n');
+			else (mul <= 15)
+			{
+				_putchar((mul / 10) + 48);
+				_putchar((mul % 10) + 48);
+			}
 		}
 	}
+	_putchar('\n');
 }
