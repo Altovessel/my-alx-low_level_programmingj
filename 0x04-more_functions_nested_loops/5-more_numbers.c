8a7568b7a5707c33456f25e6908f29bc9c7097cf
Altovessel <ogunmefunhannah2019@gmail.com>
2023-02-26 07:50:32 -0800

    Hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,16 @@
 void more_numbers(void)
 {
 	int a, n;
-	
+
 	for (a = 0; a < 10; a++)
 	{
 		for (n = 0; n < 15; n++)
 		{
-			if (n >= 10)
+			if (n < 10)
+			{
+				_putchar('n');
+			}
+			else
 			{
 				_putchar((n / 10) + 48);
 				_putchar((n % 10) + 48);
